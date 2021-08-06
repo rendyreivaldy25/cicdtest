26128be127024b469480e70c1886a6b8930c406c
rdy25 <rendyreivaldy@gmail.com>
2021-08-06 13:46:50 +0700

    pengembangan modul baru

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+
+    $version = 6;
+    $backgroundColor = "blue";
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <body style="background-color: <?= $backgroundColor ?>;">
+
+        <h1>Hello CI/CD World !</h1>
+
+        <p>This is version <?= $version ?> </p>
+
+    </body>
+</html>
